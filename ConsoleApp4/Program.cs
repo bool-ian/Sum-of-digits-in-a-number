127c582fc0d8998c1f7535463aa6e4823b1b12aa
bool-ian <59983589+bool-ian@users.noreply.github.com>
2020-02-07 15:03:17 -0500

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            SumDigit();
+      
+        }
+
+        static void SumDigit()
+        {
+
+            int num = int.Parse(Console.ReadLine());           
+            string myString = num.ToString();
+            char[] array = myString.ToCharArray();
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += int.Parse(array[i].ToString());
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+
+    }
+}
